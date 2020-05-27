1ed1ee24bc3e32abed30304f7147dfb5bee73ef1
Anacf1 <anacristina.ferreira.leite@gmail.com>
2020-05-27 17:29:43 -0300

    tudo

--- ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/SQLiteDatabase/app/src/main/java/com/example/sqlite/DbHelperSQLiteDatabase.java
+++ ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/SQLiteDatabase/app/src/main/java/com/example/sqlite/DbHelperSQLiteDatabase.java
@@ -4,17 +4,19 @@ import android.content.Context;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
+/*1. Para criar um banco de dados SQLite, precisamos implementar uma subclasse de SQLiteOpenHelper, que
+        tem por responsabilidade criar o banco de dados, assim como gerenciar a versão do mesmo.*/
 public class DbHelperSQLiteDatabase extends SQLiteOpenHelper {
 
     public static String NOME_BANCO = "contato.db";
     public static int VERSAO_BANCO = 2;
     public static String NOME_TABELA = "contatoTB";
 
-    /*Como estou trabalhando com 'SimpleCursorAdapter' (em PrincipalSQLiteDatabaseActivity.java),
-    * o framework requer que:
-    * 1. A tabela tenha um coluna '_id', que seja INTEGER PRIMARY KEY AUTOINCREMENT
-    * 2. Quando for criar o Cursor: Cursor cursor = database.query(. . . ); ,
-    *   tenha também com coluna com o nome '_id', na lista de colunas*/
+    /*Obs_01: Como estou trabalhando com 'SimpleCursorAdapter' (em PrincipalActivity.java),
+        O framework requer que:
+        * 1. A tabela tenha um coluna '_id', que seja INTEGER PRIMARY KEY AUTOINCREMENT
+        * 2. Quando for criar o Cursor: Cursor cursor = database.query(. . . ); ,
+        *   tenha também com coluna com o nome '_id', na lista de colunas*/
     public static String COLUNA_ID_CREATE_TABLE = "_id";
 
     public static String COLUNA_ID = "id";
@@ -22,7 +24,7 @@ public class DbHelperSQLiteDatabase extends SQLiteOpenHelper {
     public static String COLUNA_ENDERECO = "endereco";
     public static String COLUNA_TELEFONE = "telefone";
 
-    /*Este código so é executado uma única vez! Caso queira modificar alguma coisa no código:
+    /*Obs_02. Este código so é executado uma única vez! Caso queira modificar alguma coisa no código:
     * 1. Incrementar (+1) a versao do banco - atributo: VERSAO_BANCO
     * 2. Deletar a tabela e recriá-la*/
     public static String CREATE_TABLE = "create table " + NOME_TABELA + " (" + COLUNA_ID_CREATE_TABLE + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
@@ -39,6 +41,8 @@ public class DbHelperSQLiteDatabase extends SQLiteOpenHelper {
         db.execSQL(CREATE_TABLE);
     }
 
+    /*2. O método onUpdate() faz uso do execSQL quando a versão do banco é alterada
+        A partir de agora, toda vez que a versão do banco é alterada, o método onUpdate é executado*/
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
         db.execSQL("DROP TABLE IF EXISTS " + NOME_TABELA);
--- ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/SQLiteDatabase/app/src/main/java/com/example/sqlite/ManagerSQLiteDatabase.java
+++ ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/SQLiteDatabase/app/src/main/java/com/example/sqlite/ManagerSQLiteDatabase.java
@@ -7,6 +7,8 @@ import android.database.sqlite.SQLiteDatabase;
 
 import java.sql.SQLException;
 
+/*3. Essa classe contém métodos a serem executados nas tabelas em SQLite, como criar, atualizar, excluir,
+        selecionar etc.*/
 public class ManagerSQLiteDatabase {
     DbHelperSQLiteDatabase dbHelper;
     Context context;
@@ -27,6 +29,8 @@ public class ManagerSQLiteDatabase {
     }
 
     public void incluir(String nome, String endereco, String telefone){
+        /*4. ContentValues -> Essa classe permite definir pares de chave-valor, onde a chave representa
+            o identificador da coluna e o valor representa o conteúdo de uma coluna da tabela.*/
         ContentValues contentValues = new ContentValues();
         contentValues.put(dbHelper.COLUNA_NOME, nome);
         contentValues.put(dbHelper.COLUNA_ENDERECO, endereco);
@@ -35,6 +39,9 @@ public class ManagerSQLiteDatabase {
     }
 
     public Cursor fetch(){
+        /*5. Cursor -> A classe Cursor se assemelha ao ResultSet do JDBC.
+            Ela é capaz de referenciar as linhas resultantes de uma consulta e, além disso, possui
+            funções de navegação.*/
         String [] colunas = new String [] {dbHelper.COLUNA_ID_CREATE_TABLE, dbHelper.COLUNA_NOME, dbHelper.COLUNA_ENDERECO, dbHelper.COLUNA_TELEFONE};
         Cursor cursor = database.query(dbHelper.NOME_TABELA, colunas, null, null , null, null , null);
         if (cursor != null){
