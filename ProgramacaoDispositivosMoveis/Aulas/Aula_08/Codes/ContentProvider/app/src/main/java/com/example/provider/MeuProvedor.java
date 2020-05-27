8710a1633890060275bb04088370fa2a8bf71818
Anacf1 <anacristina.ferreira.leite@gmail.com>
2020-05-27 16:35:01 -0300

    content

--- ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/ContentProvider/app/src/main/java/com/example/provider/MeuProvedor.java
+++ ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/ContentProvider/app/src/main/java/com/example/provider/MeuProvedor.java
@@ -18,23 +18,41 @@ import java.util.HashMap;
 
 public class MeuProvedor extends ContentProvider {
 
-    private SQLiteDatabase db;
-    static final String id = "id";
-    static final String nome = "nome";
+    /*1. Essa string define a autoridade para o nosso Content Provider. Não podemos esquecer de que
+        deve ser registrado no arquivo AndroidManifest.xml. [Dominio]*/
     static final String PROVEDOR = "estacio.com.provider";
 
+    /*2. Aqui eu defino a minha URL (completa) [content://estacio.com.provider/conteudo]*/
     static final String URL = "content://" + PROVEDOR + "/conteudo";
 
-    /* Definindo a CONTENT_URI personalizada, através do método estático parse() da classe URI. */
+    /*3. Definindo a CONTENT_URI personalizada, através do método estático parse() da classe URI.
+        [content://estacio.com.provider/conteudo]*/
     static  final Uri CONTENT_URI = Uri.parse(URL);
+
     static final int uri_code = 1;
-    static UriMatcher uriMather;
+
+    static UriMatcher uriMather;  /*4. O Content Provider utiliza um UriMatcher para determinar a operação a ser efetuada em seus métodos query, insert, update e delete.*/
+
     private static HashMap values;
     static {
-        uriMather = new UriMatcher(UriMatcher.NO_MATCH);
-        uriMather.addURI(PROVEDOR, "conteudo", uri_code);
-        uriMather.addURI(PROVEDOR, "conteudo/*", uri_code);
+
+
+        uriMather = new UriMatcher(UriMatcher.NO_MATCH); /*5. A linha uriMatcher = new UriMatcher(UriMatcher.NO_MATCH) indica se certa URL é do tipo lista ou do tipo item.*/
+                                                        /*6. O parâmetro UriMatcher.NO_MATCH informa ao aplicativo qual valor padrão retornará para uma correspondência.*/
+
+        uriMather.addURI(PROVEDOR, "conteudo", uri_code); /*7. A linha uriMatcher.addURI (PROVEDOR, "conteudo", uriCode) define qualquer URL que use a autoridade
+                                                                    estacio.com.provider e tem um caminho denominado “conteúdo” retornando o valor uriCode,
+                                                                    que, em nosso exemplo, foi definido como 1.*/
+
+        uriMather.addURI(PROVEDOR, "conteudo/*", uri_code); /*8. De forma similar à anterior, a linha uriMatcher.addURI(PROVEDOR, 'conteudo/*', uriCode) define qualquer
+                                                                        URL que use a autoridade estacio.com.provider e tem um caminho parecido com “conteudo/*”, onde * é um
+                                                                        número e retornando o valor uriCode, que, em nosso exemplo, foi definido como 1.*/
+
     }
+
+    private SQLiteDatabase db;
+    /*static final String id = "id";*/
+    static final String CAMPO_NOME = "nome";
 
     @Override
     public boolean onCreate() {
@@ -60,7 +78,7 @@ public class MeuProvedor extends ContentProvider {
                 throw  new IllegalArgumentException("URI desconhecida: " + uri);
         }
         if(sortOrder == null || sortOrder == ""){
-            sortOrder = nome;
+            sortOrder = CAMPO_NOME;
             ;
         }
         Cursor c = queryBuilder.query(db, projection, selection, selectionArgs, null, null, sortOrder);
@@ -82,16 +100,15 @@ public class MeuProvedor extends ContentProvider {
     @Nullable
     @Override
     public Uri insert(@NonNull Uri uri, @Nullable ContentValues values) {
-        /*Retorna o id de nossa linha no banco.*/
+        /*11. Retorna o id de nossa linha no banco.*/
         Long rowId = db.insert(DataBaseHelper.NOME_TABELA, "", values);
         if(rowId > 0 ){
-            /*Através da linha Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID), construímos
-            um URI para um conteúdo específico, pois adiciona um id específico.*/
-            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowId);
-            /*É importante chamar o método notifyChange(). Isso fará com que as aplicações que estejam
-            utilizando esse conjunto de dados sejam notificadas, permitindo a elas atualizar também
-            os dados mostrados ao usuário.*/
-            getContext().getContentResolver().notifyChange(_uri, null);
+            Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowId); /*12. Através da linha Uri _uri = ContentUris.withAppendedId(CONTENT_URI, rowID), construímos
+                                                                          um URI para um conteúdo específico, pois adiciona um id específico.*/
+
+            getContext().getContentResolver().notifyChange(_uri, null); /*13. É importante chamar o método notifyChange(). Isso fará com que as aplicações que
+                                                                                        estejam utilizando esse conjunto de dados sejam notificadas, permitindo a elas
+                                                                                        atualizar também os dados mostrados ao usuário.*/
             return _uri;
         }else {
             throw new SQLException("Falha da inclusãodacidade na uri: " + uri);
--- ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/ContentProvider/app/src/main/java/com/example/provider/PrincipalActivity.java
+++ ProgramacaoDispositivosMoveis/Aulas/Aula_08/Codes/ContentProvider/app/src/main/java/com/example/provider/PrincipalActivity.java
@@ -22,16 +22,29 @@ public class PrincipalActivity extends AppCompatActivity {
         buttonIncluir.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
+                /*9. Quanto efetuarmos um click no botão incluir, o método incluir()
+                será chamado. Nele, criaremos o objeto do tipo ContentValues, visando encapsular o
+                par chave-valor, onde a chave é nome (campo tabela) e o valor é a cidade digitada.*/
                 ContentValues values = new ContentValues();
-                values.put(MeuProvedor.nome, (nomeCidade));
+                values.put(MeuProvedor.CAMPO_NOME, (nomeCidade));
+
+                /*10. Para acessar o Content Provider, usamos a classe ContentResolver, que pode ser
+                acessada através do método getContentResolver()*/
+
+                /*10.1 CONTENT_URI -> [content://estacio.com.provider/conteudo]
+                    Contém:
+                        10.1.1. authority = estacio.com.provider
+                        10.1.2. path = /conteudo
+                        10.1.3. scheme = content
+                        10.1.4  uriString = content://estacio.com.provider/conteudo
+                        10.1.5. values = "nome=Rio de Janeiro"*/
                 Uri uri = getContentResolver().insert(MeuProvedor.CONTENT_URI, values);
 
                 MeuProvedor meuProvedor = new MeuProvedor();
                 meuProvedor.insert(uri, values);
-
-                editTextNomeCidade.setText("");
             }
-        });
 
+        });
+        editTextNomeCidade.setText("");
     }
 }
